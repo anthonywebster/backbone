cbe07c19bcde86ec46cd6b35120980960cbf11ab
anthonywebster <anthonyanroab@gmail.com>
2015-02-05 13:49:56 -0600

    continue with server

--- server.js
+++ server.js
@@ -1,49 +1,56 @@
 
-var express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	methodOverride = require('method-override'),
-	mongoose = require('mongoose');
+var 
+express = require('express'),
+app = express(),
+bodyParser = require('body-parser'),
+methodOverride = require('method-override'),
+mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(methodOverride());
 
-varBookId = 100;
+var router = express.Router();
 
-function findBook (id) {
+//varBookId = 100;
+
+// function findBook (id) {
 	
-	for (var i = 0; i < books.length; i++) {
-		if (books[i].id===id) {
-			return books[i];
-		}
-	}
-	return null;
-}
+// 	for (var i = 0; i < books.length; i++) {
+// 		if (books[i].id===id) {
+// 			return books[i];
+// 		}
+// 	}
+// 	return null;
+// }
 
-function removeBook(id){
-	varbookIndex = 0;
-	for(var i=0; i<books.length; i++){
-		if(books[i].id === id){
-			bookIndex = i;
-		}
-	}
-	books.splice(bookIndex, 1);
-}
+// function removeBook(id){
+// 	varbookIndex = 0;
+// 	for(var i=0; i<books.length; i++){
+// 		if(books[i].id === id){
+// 			bookIndex = i;
+// 		}
+// 	}
+// 	books.splice(bookIndex, 1);
+// }
 
 
 var books = [
 {id: 98, author: 'Stephen King', title: 'The Shining', year: 1977},
 {id: 99, author: 'George Orwell', title: 1949}];
 
-app.get('/',function(request,response){
+router.get('/',function(request,response){
 	response.send("Hola");
 });
 
-app.get('/books', function (request, response) {
+router.get('/books', function (request, response) {
  
 	response.header('Access-Control-Allow-Origin', '*');
 	console.log('In GET function ');
 	response.json(books);
  
 });
+
+app.use(router);
 
 app.listen(8080);
