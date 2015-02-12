bb5c02f92db7a715a2e475bf66b1ba73d540161a
anthonywebster <anthonyanroab@gmail.com>
2015-02-12 00:26:12 -0600

    collections

--- app.js
+++ app.js
@@ -1,60 +1,72 @@
-MyModel = Backbone.Model.extend({
-	initialize:function(){
-		this.on("change",function(){
-			// console.log(JSON.stringify(this.changed));
-			// console.log('Previous attributes: ' + JSON.stringify(this.previousAttributes()));
 
-			if (this.hasChanged('name')) {
-				console.log("the name was changed");
-			}
-		});
-
-		this.on('change:name',function(e){
-			console.log("La propiedad name ha cambiado");
-		});
-	},
-	defaults:{
-		name:'anthony',
-		author:'webster'
-	},
-	printDetails:function(){
-		console.log("Test de funciones: ",this.get('name'));
-	},
-	validate: function(attrs){
-		if(attrs.year< 2000){
-			return 'Year must be after 2000';
-		}
-		if(!attrs.name){
-			return 'A name must be provided';
-		}
+var Post = Backbone.Model.extend({
+	urlRoot:'http://jsonplaceholder.typicode.com/posts',
+	parse: function(response,xhr){
+		response.title = 'prueba';
+		return response;
 	}
 });
 
-var Post = Backbone.Model.extend({
-	urlRoot:'http://jsonplaceholder.typicode.com/posts/'
-});
 
 var Posts = new Post();
 
+$('.add-post').on('click',function(){
+
+	
+	Posts.set('title','Prueba con backbone');
+
+	Posts.save(Posts.attributes,{
+		success:function(model,reponse,options){
+			console.log('Model Saved');
+			console.log(Posts.attributes);
+		},
+		error:function(model,xhr,options){
+			console.log("Failed to save model");
+		}
+	}); 
+})
+
+
 // Posts.fetch({
 // 	success:function(model,response,options){
-// 		console.log(response);
+// 		// console.log(response);
 // 	}
 // });
 
-Posts.set('title','Prueba con backbone');
 
-Posts.set('title',"que onda carlos");
 
-Posts.save(Posts.attributes,{
-	success:function(model,reponse,options){
-		console.log('Model Saved');
-		console.log(Posts.attributes);
+var Page = Backbone.Collection.extend({
+	model:Post,
+	url:'http://jsonplaceholder.typicode.com/posts',
+	wait:true,
+});
+
+Posts1 = new Post({'title':'primer array'});
+Posts2 = new Post({'title':'primer array'});
+Posts3 = new Post({'title':'primer array'});
+
+
+var data = [
+{'title':'hola'}
+];
+
+MyPage = new Page;
+
+// MyPage.add([Posts1,Posts2]);
+
+// MyPage.add([{name: "Flying Dutchman"},
+//   {name: "Black Pearl"}]);
+
+MyPage.fetch({
+	success:function(model,response,options){
+		console.log(model);
 	},
-	error:function(model,xhr,options){
-		console.log("Failed to save model");
-	}
-}); 
+});
+
+// MyPage.remove(Posts3);
+
+console.log(MyPage);
+
 
 
 
