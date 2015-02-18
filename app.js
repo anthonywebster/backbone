8e5c467b60de544a2d5efeee8fd47799dd4e2d43
anthonywebster <anthonyanroab@gmail.com>
2015-02-18 00:09:59 -0600

    vistas

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
 
 var Post = Backbone.Model.extend({
-	urlRoot:'http://jsonplaceholder.typicode.com/posts',
-	parse: function(response,xhr){
-		response.title = 'prueba';
-		return response;
-	}
+	urlRoot:'http://jsonplaceholder.typicode.com/posts'
 });
 
 
-var Page = Backbone.Collection.extend({
+var PageCollection = Backbone.Collection.extend({
 	model:Post,
 	url:'http://jsonplaceholder.typicode.com/posts',
-	wait:true,
+
 });
 
-var bookOne = new Post({name: 'Beginning Backbone', author: 'James Sugrue', year: 2013});
-
-var bookTwo = new Post({name: 'Pro Javascript Design Patterns', author:'Dustin Diaz',year: 2012});
-
-MyPage = new Page([bookOne,bookTwo]);
-
-MyPage.fetch();
-
-postviews = Backbone.View.extend({
-	collection:MyPage,
-
-	tagName:'li',
+var postviews = Backbone.View.extend({
+ 	
+	el:'.list',
 		
 	attributes:{'data-date':new Date()},
 	
-	initialize:function() {this.render(this)},
-	
-	render:function(_this){
+	initialize:function() {
+		this.render();
+		this.listenTo(this.collection,'add',this.log)		
+	},
 
-		test = {hola:"query"};
-		
-		_.forEach(this.collection,function(value,key,context,context){
-			// $(this.el).append('<li>'+context.at(key).get('name')+'</li>');
-			console.log(_this.el,this);
-		});
-		
-		// this.$el.html("<li>hola</li>");
+	events:{
+		'click .name' : 'log'
+	},
+	
+	render:function(){
+			
+		_.forEach(this.collection,function(value,key,context){
+			
+			this.$el.append('<li class="name">'+context.at(key).get('title')+'</li>');			
+		 },this);
 
 		return this;
+	},
+
+	log:function(e) {
+		console.log(this);
 	}
 
 });
 
-myView = new postviews();
+MyPage = new PageCollection();
 
-console.log(myView.el);
+MyPage.fetch({
+	success:function(model,response,options){
+		
+		myView = new postviews({collection:model});
+	}
+});
 
+var bookTwo = new Post({title: 'Pro Javascript Design Patterns', author:'Dustin Diaz',year: 2012});
 
-
+MyPage.add(bookTwo);
 
 
 
